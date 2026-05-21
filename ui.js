ce6a053b8c5bffd2dd19ae980226a4c5ad181f8d
alihae <07832021775f@gmail.com>
2026-05-21 16:18:01 +0300

    Create ui.js

---
+++ ui.js
@@ -0,0 +1,11 @@
+const startBtn =
+document.getElementById('startBtn');
+
+startBtn.addEventListener('click',()=>{
+
+  startBtn.innerText =
+  'المحاكاة تعمل...';
+
+  startBtn.style.opacity='0.8';
+
+});
